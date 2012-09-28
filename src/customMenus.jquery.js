adf3aaeb5705ecd10c696b1868bb915772d7410f
Brian McAllister <brian@brianmcallister.com>
2012-09-27 20:40:19 -0400

    Implement the clickToOpen option.

--- src/customMenus.jquery.js
+++ src/customMenus.jquery.js
@@ -16,7 +16,7 @@
       privateApi = {};
 
     // Reference to main element.
-    this.$select = $(element);
+    this.$select = $(element).addClass('m-select');
 
     // Set up options
     this.originalOptions = options || {};
@@ -66,13 +66,12 @@
 
         return this;
       },
-      
+
       setPlaceholderText: function () {
         this.updateTitle(this.getOptionData(this.getSelected()));
-        
         return this;
       },
-      
+
       bindCustomEvents: function () {
         var privateApi = this;
 
@@ -83,18 +82,61 @@
         });
 
         // Update m by changing the select box.
-        plugin.$select.on('change.m', function () {
+        plugin.$select.on('change', function () {
           var data = privateApi.getOptionData(privateApi.getSelected());
           privateApi.updateTitle(data);
         });
 
+        if (plugin.options.clickToOpen) {
+          this.clickToOpen();
+        }
+
         return this;
+      },
+
+      showList: function () {
+        plugin.$list.css('display', 'block');
+        return this;
+      },
+
+      hideList: function () {
+        plugin.$list.css('display', 'none');
+        return this;
+      },
+
+      clickToOpen: function () {
+        var $win = $(window),
+          privateApi = this;
+
+        // Clicking an li always closes the list.
+        plugin.$list.on('click.m', 'li', function () {
+          privateApi.hideList();
+          $win.off('click.m'); // Remove this handler if it exists.
+        });
+
+        plugin.$title.on('click.m', function () {
+          // Either show or hide the list.
+          if (plugin.$list.css('display') !== 'block') {
+            privateApi.showList();
+
+            setTimeout(function () {
+              $win.on('click.m', function (event) {
+                event.preventDefault(); // This is not working?
+                privateApi.hideList();
+                $win.off('click.m');
+              });
+            }, 15);
+          } else {
+            privateApi.hideList();
+            $win.off('click.m');
+          }
+        });
       },
 
       getOptionData: function ($el) {
         return {text: $el.text(), value: $el.val() || $el.data('value')};
       },
-      
+
       getSelected: function () {
         return plugin.$select.find(':selected');
       },
@@ -125,7 +167,7 @@
 
     // ...fire our custom event...
     if ($this.data('m') && value) {
-      $this.trigger('change.m');
+      $this.trigger('change');
     }
 
     // ...return the value as the old val() does.
