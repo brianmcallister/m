62f178580a30ea1248b0e4827afe6984ef306826
Brian McAllister <brian@brianmcallister.com>
2012-10-03 22:12:10 -0400

    Change arrowString option to default to true.

--- src/customMenus.jquery.js
+++ src/customMenus.jquery.js
@@ -21,7 +21,7 @@
       classPrefix: 'm', // Pass a string to set your own, or false to have nothing.
       wrapperClass: null, // Pass a string to set a class on the wrapper element. Useful for custom themes.
       clickToOpen: false, // Click the title to open the dropdown list. Behaves like a regular select box.
-      arrowString: false // Add a unicode arrow icon. Pass true, or your own unicode or even blank string.
+      arrowString: true // Add a unicode arrow icon. Pass true, or your own unicode or even blank string.
     };
     this.originalOptions = options || {};
     this.options = $.extend(this.defaults, this.originalOptions);
