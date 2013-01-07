9613c6476b5af4ee9bf212cf9406e8d72e6dec50
Brian McAllister <brian@brianmcallister.com>
2013-01-07 15:31:59 -0500

    Set up a copy task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,31 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    
+    // Concat task.
     concat: {
-      options: {
-        separator: ';'
-      },
       dist: {
         src: 'src/*.js',
-        dest: 'dist/m.jquery.js'
+        dest: 'dist/m.jquery-patched.js'
+      }
+    },
+    
+    // Copy task.
+    copy: {
+      dist: {
+        files: {
+          'dist/m.jquery.js': 'src/m.jquery.js'
+        }
       }
     }
   });
   
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   
   // Run tests?
   grunt.registerTask('default', ['concat']);
   
   
-  grunt.registerTask('build', 'Build the plugin files.', ['concat']);
+  grunt.registerTask('build', 'Build the plugin files.', ['concat', 'copy']);
 };
