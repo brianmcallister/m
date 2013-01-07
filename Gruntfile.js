e1ecbd491e242b9f0da862280c4fd54add049c44
Brian McAllister <brian@brianmcallister.com>
2013-01-07 15:08:41 -0500

    Set up a concat task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,19 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['src/m.jquery.js', 'src/m.valpatch.jquery.js'],
+        dest: 'dist/m.jquery.min.js'
+      }
+    }
   });
-
+  
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  
   // Run tests?
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['concat']);
 };
