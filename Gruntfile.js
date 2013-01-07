ef1afa5fa5a151a3f4c03190a1806931c13606b0
Brian McAllister <brian@brianmcallister.com>
2013-01-07 15:49:24 -0500

    Use the newer clean plugin.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,11 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     
+    // Clean task.
+    clean: {
+      dist: ['dist']
+    },
+    
     // Concat task.
     concat: {
       dist: {
@@ -31,6 +36,7 @@ module.exports = function(grunt) {
     }
   });
   
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
@@ -38,6 +44,8 @@ module.exports = function(grunt) {
   // Run tests?
   grunt.registerTask('default', ['concat']);
   
-  
-  grunt.registerTask('build', 'Build the plugin files.', ['concat', 'copy']);
+  // Build task.
+  grunt.registerTask('build', 'Build the plugin files.', [
+    'clean', 'concat', 'copy', 'uglify'
+  ]);
 };
