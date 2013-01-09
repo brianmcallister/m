c5d6a96a675d4a6b7adebc07176517289b0d2ba1
Brian McAllister <brian@brianmcallister.com>
2013-01-09 16:38:32 -0500

    Remove the demo directory from master.

--- demo/assets/js/lib/m.valpatch.jquery.js
+++
@@ -1,1 +0,0 @@
-../../../../src/m.valpatch.jquery.js
--- demo/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-/*jslint devel: true, browser: true, indent: 2, nomen: true */
-/*global define */
-
-define([], function () {
-  'use strict';
-
-  // console.log('it works');
-});
